607db069038c3492ad41e7ecf309b3e9ce8d6fd9
ABHISIKTA GHOSH <abhisiktaghosh2@gmail.com>
2025-01-29 16:24:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == '00') {
+            string +='00';
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'Bksp.') {
+            string = string.slice(0, -1);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == '%') {
+            string += '%';
+            document.querySelector('input').value = string;
+        }        
+        else if (e.target.innerHTML == 'x') {
+            string += '*';
+            document.querySelector('input').value = string;
+        }   
+        else {
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
